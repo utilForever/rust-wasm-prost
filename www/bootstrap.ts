a45b7c31c0c65224e1acd98226edbe31c3e7c81a
Chris Ohk <utilforever@gmail.com>
2022-03-30 02:25:23 +0900

    feat: Change code to import 'index.ts' for asynchronous load

--- www/bootstrap.ts
+++ www/bootstrap.ts
@@ -1,1 +1,5 @@
-import('./index').catch(e => console.error("Error importing `index.ts`:", e));
+await import_wasm().catch(e => console.error("Error importing `index.ts`:", e));
+
+export function import_wasm() {
+    return import("./index");
+}
