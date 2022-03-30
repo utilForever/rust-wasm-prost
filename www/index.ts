64286b53135086e32e81b3137a061b1b38a843db
Chris Ohk <utilforever@gmail.com>
2022-03-30 16:48:10 +0900

    feat: Add code to test function 'convert_proto_rust_to_ts()'

--- www/index.ts
+++ www/index.ts
@@ -1,7 +1,7 @@
 import * as wasm from "rust-wasm-prost";
 import { Student } from './proto/simple_pb';
 
-let student: Student.AsObject = {
+let student1: Student.AsObject = {
     name: "Chris",
     age: 34,
     height: 175.0,
@@ -9,7 +9,13 @@ let student: Student.AsObject = {
     gradesList: [100, 90, 85],
 };
 
-let data = JSON.stringify(student);
+let data = JSON.stringify(student1);
 console.log(data);
 
 wasm.convert_proto_ts_to_rust(data);
+
+let student2 = wasm.convert_proto_rust_to_ts();
+console.log(student2);
+
+let student3: Student.AsObject = JSON.parse(student2);
+console.log(student3);
