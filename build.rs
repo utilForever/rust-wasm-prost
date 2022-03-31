dc8b0761d6857e2e4c4f4b9da8fe2f27913c2713
Chris Ohk <utilforever@gmail.com>
2022-03-31 17:21:39 +0900

    feat: Add code to add type attributes and compile 'complex.proto'

--- build.rs
+++ build.rs
@@ -6,7 +6,27 @@ fn main() -> Result<()> {
         "rust_wasm_prost.simple.Student",
         "#[derive(serde::Serialize, serde::Deserialize)]",
     );
+    prost_build.type_attribute(
+        "rust_wasm_prost.complex.Item.Information",
+        "#[derive(serde::Serialize, serde::Deserialize)]",
+    );
+    prost_build.type_attribute(
+        "rust_wasm_prost.complex.FruitCondition",
+        "#[derive(serde::Serialize, serde::Deserialize)]",
+    );
+    prost_build.type_attribute(
+        "rust_wasm_prost.complex.VegetableCondition",
+        "#[derive(serde::Serialize, serde::Deserialize)]",
+    );
+    prost_build.type_attribute(
+        "rust_wasm_prost.complex.Item.condition",
+        "#[derive(serde::Serialize, serde::Deserialize)]",
+    );
+    prost_build.type_attribute(
+        "rust_wasm_prost.complex.Item",
+        "#[derive(serde::Serialize, serde::Deserialize)]",
+    );
     prost_build.include_file("protobuf.rs");
-    prost_build.compile_protos(&["proto/simple.proto"], &["proto/"])?;
+    prost_build.compile_protos(&["proto/simple.proto", "proto/complex.proto"], &["proto/"])?;
     Ok(())
 }
