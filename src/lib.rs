0823e1d15c82faf6342f5b2bf7de40ed24456f70
Chris Ohk <utilforever@gmail.com>
2022-03-31 17:50:23 +0900

    feat: Add function 'convert_proto_item_rust_to_ts()'

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ extern crate web_sys;
 
 use wasm_bindgen::prelude::*;
 
-use crate::rust_wasm_prost::simple::Student;
 use crate::rust_wasm_prost::complex::Item;
+use crate::rust_wasm_prost::simple::Student;
 
 // A macro to provide 'println!(..)'-style syntax for 'console.log' logging.
 #[allow(unused_macros)]
@@ -48,4 +48,48 @@ pub fn convert_proto_item_ts_to_rust(data: String) {
 
     let item: Item = serde_json::from_str(data.as_str()).unwrap();
     log!("{:?}", item);
+}
+
+#[wasm_bindgen]
+pub fn convert_proto_item_rust_to_ts1() -> String {
+    let item = Item {
+        unique_id: 201,
+        information: Some(crate::rust_wasm_prost::complex::item::Information {
+            name: "Banana".to_string(),
+            count: 4,
+            r#type: crate::rust_wasm_prost::complex::item::information::Type::Fruit as i32,
+        }),
+        condition: Some(
+            crate::rust_wasm_prost::complex::item::Condition::FruitCondition(
+                crate::rust_wasm_prost::complex::FruitCondition {
+                    brix: vec![8.8, 7.2, 9.1, 8.4],
+                    origin: "Brazil".to_string(),
+                },
+            ),
+        ),
+    };
+
+    serde_json::to_string(&item).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn convert_proto_item_rust_to_ts2() -> String {
+    let item = Item {
+        unique_id: 202,
+        information: Some(crate::rust_wasm_prost::complex::item::Information {
+            name: "Potato".to_string(),
+            count: 1,
+            r#type: crate::rust_wasm_prost::complex::item::information::Type::Vegetable as i32,
+        }),
+        condition: Some(
+            crate::rust_wasm_prost::complex::item::Condition::VegetableCondition(
+                crate::rust_wasm_prost::complex::VegetableCondition {
+                    nutrients: vec!["Vitamin A".to_string(), "Vitamin D".to_string()],
+                    origin: "China".to_string(),
+                },
+            ),
+        ),
+    };
+
+    serde_json::to_string(&item).unwrap()
 }
