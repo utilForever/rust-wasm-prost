4f830c0149acc3a784bab901c98f464c27f77fd6
Chris Ohk <utilforever@gmail.com>
2022-03-31 17:26:59 +0900

    feat: Add code to test function 'convert_proto_student_ts_to_rust()'

--- www/index.ts
+++ www/index.ts
@@ -1,5 +1,6 @@
 import * as wasm from "rust-wasm-prost";
 import { Student } from './proto/simple_pb';
+import { Item } from './proto/complex_pb';
 
 let student1: Student.AsObject = {
     name: "Chris",
@@ -9,13 +10,49 @@ let student1: Student.AsObject = {
     gradesList: [100, 90, 85],
 };
 
-let data = JSON.stringify(student1);
-console.log(data);
+let data_student = JSON.stringify(student1);
+console.log(data_student);
 
-wasm.convert_proto_student_ts_to_rust(data);
+wasm.convert_proto_student_ts_to_rust(data_student);
 
 let student2 = wasm.convert_proto_student_rust_to_ts();
 console.log(student2);
 
 let student3: Student.AsObject = JSON.parse(student2);
 console.log(student3);
+
+let item1: Item.AsObject = {
+    uniqueId: 100,
+    information: {
+        name: "Apple",
+        count: 3,
+        type: Item.Information.Type.FRUIT,
+    },
+    fruitCondition: {
+        brixList: [12.9, 10.4, 11.3],
+        origin: "Korea",
+    },
+};
+
+let item2: Item.AsObject = {
+    uniqueId: 101,
+    information: {
+        name: "Onion",
+        count: 5,
+        type: Item.Information.Type.VEGETABLE,
+    },
+    vegetableCondition: {
+        nutrientsList: ["Vitamin B", "Vitamin C"],
+        origin: "Korea",
+    },
+};
+
+let data_item1 = JSON.stringify(item1);
+console.log(data_item1);
+
+wasm.convert_proto_item_ts_to_rust(data_item1);
+
+let data_item2 = JSON.stringify(item2);
+console.log(data_item2);
+
+wasm.convert_proto_item_ts_to_rust(data_item2);
