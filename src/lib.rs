d57180d0c8684c710e9a935186e906b925a70f70
Chris Ohk <utilforever@gmail.com>
2022-04-05 16:36:37 +0900

    feat: Add function 'convert_raw_bytes_item_rust_to_ts()'

--- src/lib.rs
+++ src/lib.rs
@@ -118,3 +118,47 @@ pub fn convert_raw_bytes_item_ts_to_rust(data: &[u8]) {
     let item: Item = prost::Message::decode(data).expect("failed to decode protobuf binary");
     log!("{:?}", item);
 }
+
+#[wasm_bindgen]
+pub fn convert_raw_bytes_item_rust_to_ts1() -> Vec<u8> {
+    let item = Item {
+        unique_id: 201,
+        information: Some(crate::rust_wasm_prost::complex::item::Information {
+            name: "Banana".to_string(),
+            count: 4,
+            r#type: crate::rust_wasm_prost::complex::item::information::Type::Fruit as i32,
+        }),
+        condition: Some(
+            crate::rust_wasm_prost::complex::item::Condition::FruitCondition(
+                crate::rust_wasm_prost::complex::FruitCondition {
+                    brix: vec![8.8, 7.2, 9.1, 8.4],
+                    origin: "Brazil".to_string(),
+                },
+            ),
+        ),
+    };
+
+    prost::Message::encode_to_vec(&item)
+}
+
+#[wasm_bindgen]
+pub fn convert_raw_bytes_item_rust_to_ts2() -> Vec<u8> {
+    let item = Item {
+        unique_id: 202,
+        information: Some(crate::rust_wasm_prost::complex::item::Information {
+            name: "Potato".to_string(),
+            count: 1,
+            r#type: crate::rust_wasm_prost::complex::item::information::Type::Vegetable as i32,
+        }),
+        condition: Some(
+            crate::rust_wasm_prost::complex::item::Condition::VegetableCondition(
+                crate::rust_wasm_prost::complex::VegetableCondition {
+                    nutrients: vec!["Vitamin A".to_string(), "Vitamin D".to_string()],
+                    origin: "China".to_string(),
+                },
+            ),
+        ),
+    };
+
+    prost::Message::encode_to_vec(&item)
+}
